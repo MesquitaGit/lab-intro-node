dce78f35009901d19905ad5f2d98ba36d74954ec
João Mesquita <joaopsmesquita@gmail.com>
2022-11-03 11:51:01 +0000

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+    throw new Error('OutOfBounds');
+    }else{
+    return this.items[pos];
+    }
+  };
 
-  max() {}
+  max() {
 
-  min() {}
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
 
-  sum() {}
+    const max = this.items.reduce((a, b) => { return Math.max(a, b) });
+    return max;
+  }
 
-  avg() {}
+  min() {
+
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    const min = this.items.reduce((a, b) => { return Math.min(a, b) });
+    return min;
+  }
+
+  sum() {
+
+    if(this.items.length === 0){
+      return 0;
+    }
+
+    let sum = this.items.reduce(function (previousValue, currentValue) {
+      return previousValue + currentValue;
+  });
+    return sum;
+  }
+
+  avg() {
+
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    let sum = this.items.reduce(function (previousValue, currentValue) {
+      return previousValue + currentValue;
+  });
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
